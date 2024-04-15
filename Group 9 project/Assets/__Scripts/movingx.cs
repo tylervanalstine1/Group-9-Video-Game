20b6b8b3799634d583f684acfa685faa015a87fe
Nutsomethingserious <kll0032@uah.edu>
2024-04-15 11:29:42 -0500

    added move, feel free to pull now fixed earlier issue
    added objects that can move

--- Group 9 project/Assets/__Scripts/movingx.cs
+++ Group 9 project/Assets/__Scripts/movingx.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoundPlayer : MonoBehaviour
+public class movingx: MonoBehaviour
 {
     // speed -Knl
     public float speed = 1f;
